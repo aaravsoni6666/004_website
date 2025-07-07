234e2974c7cd57fc9222aee99243a672240778f4
aaravsoni6666 <emortal790@gmail.com>
2025-07-07 16:58:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  document.getElementById("mainNav").classList.toggle("show");
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("mainNav");
+  nav.classList.toggle("show");
+}
